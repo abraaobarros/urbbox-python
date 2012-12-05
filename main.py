c6f310b769978aeb97687a34acea6ff5225f3708
abraaobarros3 <abraaobarros3@gmail.com>
2012-12-05 06:39:46 -0200

    Projeto appEngine Básico criado

--- main.py
+++ main.py
@@ -15,60 +15,10 @@
 # limitations under the License.
 #
 import webapp2
-from models import Pedido
-from models import UsuarioEstabelecimento
-import json
-
 
 class MainHandler(webapp2.RequestHandler):
-  def get(self):
-    #u = UsuarioEstabelecimento()
-    #u.email = "kenta"
-    #u.idEstalebecimento = [40]
-    #u.password = "asd"
-    #u.put()
-    p = Pedido()
-    p.idEstabelecimento = 100
-    p.itens="{nada}"
-    p.rua = "Mesa 10"
-    p.status = "Waiting"
-    p.put()
-    lista = Pedido.all()
-    count = 0
-    for p in lista:
-      print "a"
-    lista2 = UsuarioEstabelecimento.all()
-    count = 0
-    #for u in lista2:
-    #  #u.id_est = u.idEstalebecimentoList[0]
-    #  #u.put()
-    #  self.response.out.write("parser do id do Estabelecimento %d" %u.idEstabelecimentoList.pop())
-    #  
-class UserHandler(webapp2.RequestHandler):
-  def get(self):
-    login = self.request.get('login')
-    password = self.request.get('password')
-    u = UsuarioEstabelecimento.all()
-    user = u.filter('email =' , login).get()
-    if user:
-      if user.password == password:
-        self.response.out.write("{'authentication':'true','idStore':%d}" %user.idEstabelecimentoList.pop())
-      else:
-        self.response.out.write("{'authentication':false,'error':1,'msg':'Password Incorreto'}")
-    else:
-      self.response.out.write("{'authentication':false,'error':0,'msg':'Usuario nao existe'}")
-     
-class PedidoHandler(webapp2.RequestHandler):
-  def get(self,est_id):
-    lista = Pedido.all()
-    lista.filter('idEstabelecimento =', long(est_id))
-    count = 0
-    for p in lista.run():
-     count+=1
-    #  count+=1
-    self.response.out.write("{'qtd':%d}" %count)
+    def get(self):
+        self.response.out.write('Hello world!')
 
-app = webapp2.WSGIApplication([
-    (r'/pedidos/(\d+)', PedidoHandler)
-    ,(r'/user', UserHandler)
-    ,('/', MainHandler)], debug=True)
+app = webapp2.WSGIApplication([('/', MainHandler)],
+                              debug=True)
