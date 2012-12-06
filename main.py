effba1224e920c456fcf9df4afbbd6703af41e9e
Marcelo Almeida <marcelocralmeida@gmail.com>
2012-12-06 16:04:47 -0200

    persistindo no banco. tem que fazer imprimir o que está no banco agora

--- main.py
+++ main.py
@@ -2,15 +2,38 @@
 import webapp2
 import jinja2
 import os
+from google.appengine.ext import db
+
+class Estabelecimento(db.Model):
+    nome = db.StringProperty()
+    descricao = db.StringProperty()
+    logoURL = db.StringProperty()
+
 
 env = jinja2.Environment(loader = 
 jinja2.FileSystemLoader(os.path.dirname(__file__)))
 
-
 class MainHandler(webapp2.RequestHandler):
     def get(self):
-        template = env.get_template('view/index.html')
-        self.response.out.write(template.render(var='variables'))
+    	inserir()
+    	estabelecimentos = db.GqlQuery(
+                            "SELECT * "
+    						"FROM Estabelecimento "
+    						"WHERE ANCESTOR IS :1 "
+    						"ORDER BY nome ASC", 
+    						db.Key.from_path('Estabelecimento','nome'))
+    	template = env.get_template('view/index.html')
+        for estabelecimento in estabelecimentos:
+            self.response.out.write(estabelecimento.nome)
+        self.response.out.write(template.render(var=estabelecimentos))
+
+def inserir():
+	for i in range(10):	
+		estab = Estabelecimento()
+		estab.nome = "teste"+str(i)
+		estab.descricao = "teste"+str(i)
+		estab.logoURL = "teste"+str(i)
+		estab.put()
 
 app = webapp2.WSGIApplication([('/', MainHandler)],
 debug=True)
