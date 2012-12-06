eb5976df8aa251860824386a8c5e6b09b78f82a4
abraaobarros3 <abraaobarros3@gmail.com>
2012-12-06 03:45:46 -0200

    Layout para a lista de restaurantes+ configuraçao de testes

--- main.py
+++ main.py
@@ -3,13 +3,14 @@ import webapp2
 import jinja2
 import os
 
-env = jinja2.Environment(loader=jinja2.FileSystemLoader(os.path.dirname(__file__)))
+env = jinja2.Environment(loader = 
+jinja2.FileSystemLoader(os.path.dirname(__file__)))
 
 
 class MainHandler(webapp2.RequestHandler):
     def get(self):
-    	template = env.get_template('view/index.html')
-    	self.response.out.write(template.render(var='variables'))
+        template = env.get_template('view/index.html')
+        self.response.out.write(template.render(var='variables'))
 
 app = webapp2.WSGIApplication([('/', MainHandler)],
-                              debug=True)
+debug=True)
---
+++ tests/test.py
@@ -0,0 +1,20 @@
+import unittest
+
+def isOdd(n):
+    return n % 2 == 1
+
+
+class isOddTest(unittest.TestCase):
+    def testOne(self):
+        self.failUnless(isOdd(1))
+        
+    def testTwo(self):
+        self.failIf(isOdd(2))
+
+def main():
+	unittest.main()
+
+if __name__ == '__main__':
+	main()
+
+		
