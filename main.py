ef167a0e1b2ace5ecf4854520ff12e482467d56c
abraaobarros3 <abraaobarros3@gmail.com>
2012-12-07 04:36:17 -0200

    Integracao do template com o banco de dados

--- main.py
+++ main.py
@@ -1,18 +1,31 @@
-
 import webapp2
 import jinja2
 import os
+from google.appengine.ext import db
+
+class Estabelecimento(db.Model):
+    nome = db.StringProperty()
+    descricao = db.StringProperty()
+    logoURL = db.StringProperty()
+
 
 env = jinja2.Environment(loader = 
 jinja2.FileSystemLoader(os.path.dirname(__file__)))
 
-
 class MainHandler(webapp2.RequestHandler):
     def get(self):
-        template = env.get_template('view/index.html')
-        self.response.out.write(template.render(est="Doidera"))
+        inserir()  # para fazer funcionar tem que inserir alguma coisa no banco
+    	estabelecimentos = Estabelecimento.all()
+    	template = env.get_template('view/index.html')
+        self.response.out.write(template.render(var=estabelecimentos))
 
+def inserir():
+	for i in range(10):	
+		estab = Estabelecimento()
+		estab.nome = "teste"+str(i)
+		estab.descricao = "teste"+str(i)
+		estab.logoURL = "teste"+str(i)
+		estab.put() 
 
-	
-app = webapp2.WSGIApplication([('/', MainHandler)],
+app = webapp2.WSGIApplication([('/', MainHandler) ],
 debug=True)
--- tests/test.py
+++ tests/test.py
@@ -9,9 +9,12 @@ class isOddTest(unittest.TestCase):
         self.failUnless(isOdd(1))
     def testTwo(self):
         self.failIf(isOdd(2))
+        self.
 
 def main():
 	unittest.main()
+
+
 
 if __name__ == '__main__':
 	main()
